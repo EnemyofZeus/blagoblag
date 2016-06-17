7ceea9251c958fb16fc18986c17bbc4d6b4900a7
Kyle Hamburg <kylelhamburg@gmail.com>
2016-06-17 17:50:12 -0600

    Started on functions to read blog posts

--- components/blog.js
+++ components/blog.js
@@ -1,5 +1,6 @@
 var React = require('react');
 var NewBlogPost = require('./newBlogPost.js');
+var GetBlogs = require('./getBlogs.js');
 
 var Blog = React.createClass({
   getInitialState: function(){
@@ -18,6 +19,7 @@ var Blog = React.createClass({
         <h1>Here is where the actual blogging will occur.</h1>
         <h2 onClick={this.newPost}>Because seriously, why would I put this on the front page</h2>
         {this.state.showForm ? <NewBlogPost /> : null}
+        <GetBlogs />
       </div>
     )
   }
--- config.js
+++ config.js
@@ -2,5 +2,3 @@ module.exports = {
   port: process.env.PORT || 3000,
   mongo_uri: process.env.MONGOLAB_URI
 };
-
-//mongodb://<admin>:<admint>@ds017514.mlab.com:17514/blagoblag
